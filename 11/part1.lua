8c3cb26bfc3de4f083044d11868bb8527c745f95
Dimitri Tuaz <dimitri.tuaz@meetlima.com>
2018-12-19 15:04:49 +0100

    day 11

---
+++ 11/part1.lua
@@ -0,0 +1,45 @@
+local serial_number = 7400
+local max = 300
+local subgrid_size = 3
+
+local function print_subgrid(grid, xa, xb, ya, yb)
+    for j=ya, yb do
+        local line = ""
+            for i=xa, xb do
+            line = line .. string.format("%2d",grid[i][j]) .. " "
+        end
+        print(line)
+    end
+end
+
+local power_grid = {}
+local max_power = -math.huge
+local x_max_power = 1
+local y_max_power = 1
+
+for i=1,max do
+    power_grid[i] = {}
+    for j=1,max do
+        local power_level = (((i + 10) * j) + serial_number) * (i + 10)
+        power_level = tonumber(tostring(power_level):sub(-3, -3)) - 5
+        power_grid[i][j] = power_level
+    end
+end
+
+for i=1,max-subgrid_size do
+    for j=1,max-subgrid_size do
+        local total_power = 0
+        for k=0,subgrid_size-1 do
+            for l=0,subgrid_size-1 do
+                total_power = total_power + power_grid[i + k][j + l]
+            end
+        end
+        if total_power > max_power then
+            max_power = total_power
+            x_max_power = i
+            y_max_power = j
+        end
+    end
+end
+
+print(string.format("<%3d,%3d> (%d)", x_max_power, y_max_power, max_power))
